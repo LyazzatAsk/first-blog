06c315ef7be8706576b36d1b636e587b44da5d59
Lyazzat Askarova <askarovalyazzat@gmail.com>
2018-12-22 19:47:36 +0500

    urls added

---
+++ blog/templates/blog/news.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#flip").click(function() {
+    $("#panel").slideToggle("slow");
+  });
+
+  $("#flip2").click(function() {
+    $("#panel2").slideToggle("slow");
+  });
+
+  $("#flip3").click(function() {
+    $("#panel3").slideToggle("slow");
+  });
+
+  $("button").on("click", function() {
+    $(".overlay").addClass("active");
+  });
+
+  $(".tab-list").on("click", ".tab", function(e) {
+    e.preventDefault();
+    $(".tab").removeClass("active");
+    $(".tab-content").removeClass("show");
+    $(this).addClass("active");
+    $($(this).attr("href")).addClass("show");
+  });
+});
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+def story(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/stories.html', {'posts': posts})
+
+def index(request):
+    return render(request, 'blog/index6.html')
+
+
+def gallery(request):
+    return render(request, 'blog/gallery.html')
+
+def form(request):
+    return render(request, 'blog/formdis.html')
+
+
+def new(request):
+    return render(request, 'blog/news.html')
+
 
 # Create your views here.
